7046992a7920a9f46548d5204a56fd11d2718d5d
Euizzang8001 <qkrdmlcks55@naver.com>
2023-11-06 23:32:43 +0900

    week4

--- week4/dags/src/stock_algo.py
+++ week4/dags/src/stock_algo.py
@@ -1,13 +1,16 @@
 import numpy as np
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 from pykrx import stock
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
+import pendulum
 
 def get_today():
-    dt_now = str(datetime.now().date())
+    kst = pendulum.timezone('Asia/Seoul')
+    current_time = datetime.now().astimezone(kst)
+    dt_now = str(current_time.date())
     print(f'{dt_now} 기준')
     dt_now = ''.join(c for c in dt_now if c not in '-')
     return dt_now
@@ -26,26 +29,27 @@ def nexon_xy():
     scale_cols_for_y = ['종가']
     test = scaler.fit_transform(nexon[scale_cols_for_x])
     test2 = scaler2.fit_transform(nexon[scale_cols_for_y])
+    np.save(f'./{dt_now}_test.npy', test)
+    np.save(f'./{dt_now}_test2.npy', test2)
+    
+       
+def lstm_nexon(today_info):
+    dt_now = get_today()
+    test = np.load(f'./{dt_now}_test.npy')
+    test2 = np.load(f'./{dt_now}_test2.npy')
     X = np.array([test[i:i+1] for i in range(test.shape[0]-1 )])
     y = np.array([test2[i+1] for i in range(test2.shape[0]-1)])
-    return X, y
-
-def nexon_prep():
-    X, y = nexon_xy()
     X_model_input = tf.keras.layers.Input(shape=(X.shape[1], X.shape[2]))
     X_model_output = tf.keras.layers.LSTM(32, activation='relu', return_sequences=False)(X_model_input)
     X_model_output = tf.keras.layers.Dense(1, activation='linear')(X_model_output)
     X_model_output = tf.keras.layers.Dense(1)(X_model_output)
-    
-def lstm_nexon():
-    X, y = nexon_xy(0)
-    X_model_input, X_model_output = nexon_prep()
     x_train, x_valid, y_train, y_valid = train_test_split(X, y, test_size = 0.2) 
     result_model = tf.keras.Model(inputs=X_model_input, outputs=X_model_output)
     result_model.compile(optimizer='adam', loss='mean_squared_error', metrics=['acc'])
     early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
     result_model.fit(x_train, y_train, batch_size = 1, epochs=100, validation_data=(x_valid, y_valid), callbacks=[early_stop])                
-    return result_model
+    pred = result_model.predict(np.array([today_info]))
+    return pred
 
 def predict_or_check():
     dt_now = get_today()
@@ -54,19 +58,19 @@ def predict_or_check():
     scaler2 = MinMaxScaler()
     scale_cols_for_x = ['시가', '고가', '저가', '거래량']
     scale_cols_for_y = ['종가']
-    test = scaler.fit_transform(nexon[scale_cols_for_x])
-    test2 = scaler2.fit_transform(nexon[scale_cols_for_y])
+    scaler.fit_transform(nexon[scale_cols_for_x])
+    scaler2.fit_transform(nexon[scale_cols_for_y])
 
-    result_model = lstm_nexon()
-    dt_now = get_today()
-    current_time = datetime.now()
+    kst = pendulum.timezone('Asia/Seoul')
+    current_time = datetime.now().astimezone(kst)
+
     today_nexon = stock.get_market_ohlcv(dt_now, dt_now, "225570")
-    today_info = scaler.transform([today_nexon[i].values[0] for i in ['시가', '고가', '저가', '거래량']])
-    test = result_model.predict(np.array([today_info]))
+    today_info = scaler.transform([[today_nexon['시가'].values[0], today_nexon['고가'].values[0], today_nexon['저가'].values[0], today_nexon['거래량'].values[0]]])
+    test = lstm_nexon(today_info=today_info)
     today_pred = scaler2.inverse_transform(test)
     today_pred = "{:.2f}".format(today_pred[0][0])
     if current_time.hour >= 15:
         today_close = today_nexon['종가'].values[0]
-        print(f'넥슨!\n오늘의 예측 종가는?:\n{today_pred}원\n오늘의 진짜 종가!:\n{today_close}')
+        print(f'넥슨!\n{dt_now}의 예측 종가는?:\n{today_pred}원\n오늘의 진짜 종가!:\n{today_close}')
     else:
-         print(f'넥슨!\n오늘의 예측 종가는?:\n{today_pred}원')
+         print(f'넥슨!\n{dt_now}의 예측 종가는?:\n{today_pred}원')
--- week4/dags/stock.py
+++ week4/dags/stock.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-from src.stock_algo import get_today, get_nexon, nexon_xy, nexon_prep, lstm_nexon, predict_or_check
+from src.stock_algo import get_today, get_nexon, nexon_xy, lstm_nexon, predict_or_check
 
 
 # from src.stock_algo import
@@ -21,7 +21,7 @@ with DAG(
     dag_id=dag_name,
     default_args=default_args,
     description='어려우어어어어어어어어',
-    schedule_interval=timedelta(minutes=10),
+    schedule_interval=timedelta(hours=12),
     start_date = pendulum.datetime(2023, 11, 6, tz=kst),
     catchup=False,
     tags=['stock', 'Euizzang']
@@ -34,16 +34,8 @@ with DAG(
         task_id = 'nexon_xy_task',
         python_callable=nexon_xy,
     )
-    nexon_prep_task = PythonOperator(
-        task_id='nexon_prep_task',
-        python_callable=nexon_prep,
-    )
-    lstm_nexon_task = PythonOperator(
-        task_id='lstm_nexon_task',
-        python_callable= lstm_nexon,
-    )
     predict_or_check_task = PythonOperator(
         task_id = 'predict_or_check_task',
         python_callable=predict_or_check,
     )
-    get_nexon_task >> nexon_xy_task >> nexon_prep_task >> lstm_nexon_task >> predict_or_check_task
+    get_nexon_task >> nexon_xy_task >> predict_or_check_task
